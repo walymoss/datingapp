11c2e21b0ef8f6d995f4aa48f7e5afb473de8c0c
Walter Estrada <walter.estrda.r@outlook.com>
2025-12-03 08:08:44 -0500

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,9 +9,12 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+   .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.MapControllers();
-
 app.Run();
